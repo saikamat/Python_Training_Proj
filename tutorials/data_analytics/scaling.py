e8917bad8f7aafe6c3f2b506fc1df5c102d455be
Sai <sai.s.kamat@gmail.com>
2018-06-27 19:56:40 +0200

    plots

---
+++ tutorials/data_analytics/scaling.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+#import the csv files.
+x_train = pd.read_csv(
+    r'C:\Users\Sai Kamat\source\repos\Python_for_DS\Datasets\loan_prediction\X_train.csv')
+y_train = pd.read_csv(
+    r'C:\Users\Sai Kamat\source\repos\Python_for_DS\Datasets\loan_prediction\Y_train.csv')
+x_test = pd.read_csv(
+    r'C:\Users\Sai Kamat\source\repos\Python_for_DS\Datasets\loan_prediction\X_test.csv')
+y_test = pd.read_csv(
+    r'C:\Users\Sai Kamat\source\repos\Python_for_DS\Datasets\loan_prediction\Y_test.csv')
+
+#print(x_train.head())
+
+#feature scaling
+import matplotlib.pyplot as plt
+
+
+x_train[x_train.dtypes[(x_train.dtypes == 'float64')|(x_train.dtypes == 'int64')].index.values].hist(figsize=[11,11])
+plt.show()
