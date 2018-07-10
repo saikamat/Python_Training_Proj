c91e1253efd1fee6ad1efb17931209a4e5ea059d
Sai <sai.s.kamat@gmail.com>
2018-07-10 11:23:44 +0200

    denoising & normalizing

--- tutorials/data_analytics/datasetting.py
+++ tutorials/data_analytics/datasetting.py
@@ -9,6 +9,7 @@
 import pandas as pd
 import glob
 import os
+import numpy as np
 def concatenateCSV(indir = r'C:\Users\Sai Kamat\Downloads\CSVs', outfile = r'D:\DDOS\Y.csv', outfileX = r'D:\DDOS\X.csv'):
     os.chdir(indir)
     filelist = glob.glob('*.csv')
@@ -22,13 +23,29 @@ def concatenateCSV(indir = r'C:\Users\Sai Kamat\Downloads\CSVs', outfile = r'D:\
         dfList.append(df)
         dfX = pd.read_csv(filename, usecols = colsX, encoding='cp1252')
         dfXList.append(dfX)
-    concatenated_data_frame = pd.concat(dfList, axis = 0)
-    concatenated_data_frame.columns = cols
-    concatenated_data_frame.to_csv(outfile, index = None)
-    concatenated_data_frameX = pd.concat(dfXList, axis = 0)
-    concatenated_data_frameX.columns = colsX
-    concatenated_data_frameX.to_csv(outfileX, index = None)
-concatenateCSV()
+    #concatenated_data_frame = pd.concat(dfList, axis = 0)
+    #concatenated_data_frame.columns = cols
+    #concatenated_data_frame.to_csv(outfile, index = None)
+    #concatenated_data_frameX = pd.concat(dfXList, axis = 0)
+    #concatenated_data_frameX.columns = colsX
+    #concatenated_data_frameX.to_csv(outfileX, index = None)
+#    return (concatenated_data_frame, concatenated_data_frameX)
+
+    ## convert the dataframe of float into a numpy float matrix.
+    ##dfXList_np = np.matrix(dfXList).astype(np.float32)
+##concatenateCSV()
+
+#def vectorize():
+#    x_data_path = r'D:\DDOS\abc.csv'
+#    y_data_path = r'D:\DDOS\Y.csv'
+#    x_data_np = np.genfromtxt(x_data_path, dtype=float)
+
+#vectorize()
+
+
+
+
+
 ##3.3 concatenating mulptiple CSV's 
 #def concatenateCSV(indir = r'D:\DDOS', outfile = r'D:\DDOS\result.csv'):
 #    os.chdir(indir)
